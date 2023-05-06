ad80aa2059055191701b7678d2b8724cc163ae99
LeonardoCamargo77 <leonardocamargx@gmail.com>
2023-05-05 23:00:36 -0300

    Aluno que deu erro

--- js/script.js
+++ js/script.js
@@ -135,3 +135,33 @@
 // frutas.splice(indice,1);
 // console.log(frutas);
 // console.log(`O item encontrado no índice depois da remoção foi: ${ frutas[indice]}`);
+
+
+
+// //Concatenando os dois arrays em um novo array.
+// let nr3 = [nr1,nr2];
+// console.log("Array 3 : " + nr3);
+
+// nr3;forEach( (nr)=>{
+//     nr.forEach((n)=>{
+//         console.log("Itens do Array 3 : " + n);
+//     });
+ 
+
+
+// } );
+
+//Concatenação de Arrays
+let nr1 = [1,2,3,4,5];
+let nr2 = [6,7,8,9,10];
+// console.log("Array 1 : " + nr1);
+// console.log("Array 2 : " + nr2);
+
+//Concatenando os dois arrays em um novo array com SPREAD( ... )
+//      
+console.log("Array 3 : " + nr3);
+
+// nr3.forEach( (nr)=>{
+    
+//         console.log("Itens do Array 3 : " + nr);
+// } );
