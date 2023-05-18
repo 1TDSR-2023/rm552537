2dab1ff028097787e4b7ea5ff97bbf03cf8beb78
LeonardoCamargo77 <leonardocamargx@gmail.com>
2023-05-17 23:05:04 -0300

    aula17.05.2023

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 const inputEmail = document.querySelector("#idEmail");
 
-inputEmail.addEventListener("focus", ()=>{
-     inputEmail.setAttribute("style","outline-color:#ff0000;");
+    inputEmail.addEventListener("focus", ()=>{
+    inputEmail.setAttribute("style","outline-color:#ff0000;");
 
 })
 
@@ -39,11 +39,73 @@ inputEmail.addEventListener("keyup", ()=>{
     });
 
 
+    const usuario = {
+        nomeCompleto : "Lucas",
+        emailUsuario : "lucas@gmail",
+        senhaUsuario : "12345"
+    }
 
-    console.log(inputEmail.value.lengt);
-   
-   
-   
-    // inputEmail.setAttribute("style","outline-color:#ff0000;");
+    const usuario2 = {
+        nomeCompleto : "Leo",
+        emailUsuario : "leo@gmail",
+        senhaUsuario : "54321"
+    }
 
-})
+    let listadeUsuarios = [];
+    listadeUsuarios.push(usuario1);
+    listadeUsuarios.push(usuario2);
+
+
+    const botaoSumit = document.querySelector("#btnSubmit");
+
+    botaoSumit.addEventListener("click", ()=>{
+
+
+        //Criando um Ojeto de Validação
+        const userValidado = {
+            emailUsuarioValidado : inputEmail.value,
+            senhaUsuarioValidado : inputSenha.value
+        }
+
+
+        //Criando uma interação com a lista de objetos para realizar a validação com o usuárioValidado
+        listadeUsuarios.forEach((usuario)=>{
+            //Realizando a validação
+            //Comparando cada objeto da lista com o objeto montado com os dados que vieram do formulário
+
+            if(userValidado.emailUsuarioValidado == usuario.email && userValidado.senhaUsuarioValidado == usuario.senhaUsuario){
+                userValidado = usuario 
+                throw "VALIDADO";
+            }
+
+        });
+
+        throw "SENHA OU USUARIO INVÁLIDOS!";
+
+}   catch(msg){
+
+        const msgStatus = document.querySelector("#msgStatus");
+
+        if(msg == "VALIDADO"){
+            msg.Status.setAttribute("style","color:#00ff00");
+            msgStatus.innerHTML = "<span><strong>Usuário: ${userValidado.nomeCompleto}</strong></span>";
+    
+
+        //Redirect
+        setTimeout(()=>{
+            window.location.href = "../home.html";
+
+        }, 3000);
+
+        
+    
+    }else{
+        msg.Status.setAttribute("style","color:#ff0000");
+        msg.Status.innerHTML = "<span><strong>Nome de usuário ou senha inválidos</strong></span>";
+    }
+
+
+  
+}
+
+});
